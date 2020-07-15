6f43adcecd8c9825011cfe2e9c3a349d0a750fb5
jq2st <igaaaaan7@gmail.com>
2020-07-15 19:42:04 +0300

    header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { AdminLayoutComponent } from './admin/admin-layout/admin-layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainLayoutComponent, children: [
+  ]},
+  {path: 'admin', component: AdminLayoutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AdminLayoutComponent } from './admin/admin-layout/admin-layout.componen
 import { AdminNavComponent } from './admin/admin-nav/admin-nav.component';
 import { AdminHeaderComponent } from './shared/components/admin-header/admin-header.component';
 import { AdminLoginPageComponent } from './admin/admin-login-page/admin-login-page.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { AdminLoginPageComponent } from './admin/admin-login-page/admin-login-pa
     AdminLayoutComponent,
     AdminNavComponent,
     AdminHeaderComponent,
-    AdminLoginPageComponent
+    AdminLoginPageComponent,
+    MainLayoutComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
